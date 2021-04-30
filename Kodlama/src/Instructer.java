d5b8838ceb448617957db6b4818456dc753a4622
Ezgi <78070798+ekiciezgi@users.noreply.github.com>
2021-05-01 00:12:41 +0200

    1nisan.

---
+++ Kodlama/src/Instructer.java
@@ -0,0 +1,19 @@
+
+
+public class Instructer extends User{
+
+	
+	private int InsCourses;
+
+
+	public Instructer(String name, String email, int password,int InsCourses) {
+		super(name, email, password);
+		this.InsCourses=InsCourses;
+	
+	}
+	   
+	public int getInsCourses() {
+		return InsCourses;
+	}
+
+}
---
+++ Kodlama/src/UserManager.java
@@ -0,0 +1,10 @@
+
+public class UserManager {
+	
+	public void addUser(User user) {
+	
+		System.out.println("addUser çalýþtý.Kullanýcý eklendi.");
+	}
+	
+  
+}
